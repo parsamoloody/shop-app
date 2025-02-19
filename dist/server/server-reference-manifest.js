bac2c4aa0b7a2f0031f98a5a9f533acc301f7757
parsamoloody <parsaprgramerjs@gmail.com>
2025-02-19 18:35:48 +0330

    config

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3eOT2oDaVMgYqRDvbMww8RLEH68NQy4ptvNKJPXwVhI=\"\n}"
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  // distDir: 'dist', // Set the output directory
-  // output: 'export', // Configure for static export (e.g., `next export`)
+  distDir: 'dist', // Set the output directory
+  output: 'export', // Configure for static export (e.g., `next export`)
   
   webpack: (config) => {
     config.resolve.alias['@assets'] = path.join(__dirname, 'public/assets');
